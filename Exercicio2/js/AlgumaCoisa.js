889e14e88cc688d1c974919ccf8ba208b97073db
MatheusNunes02 <88735983+MatheusNunes02@users.noreply.github.com>
2021-09-21 18:04:20 -0300

    tabuada

--- Exercicio2/js/AlgumaCoisa.js
+++
@@ -1,9 +0,0 @@
-let x = Number(prompt("Digite um número"));
-
-if(x<0){
-    document.write("O número é negativo!");
-}else if(x>0){
-    document.write("O número é positivo!");
-}else{
-    document.write("O número é neutro");
-}
---
+++ Exercicio2/js/Tabuada5.js
@@ -0,0 +1,3 @@
+for(i=1; i<11; i++){
+    document.write(`5 x ${i} = ${5*i} <br> <br>`);
+}   
