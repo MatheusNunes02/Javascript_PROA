d9f398836c1c8b54f664d1ab302cc6d8eb8f18c5
MatheusNunes02 <88735983+MatheusNunes02@users.noreply.github.com>
2021-09-21 17:53:17 -0300

    Exercicios

---
+++ Exercicio1/js/NumeroMaiorMenorZero.js
@@ -0,0 +1,9 @@
+let x = Number(prompt("Digite um número"));
+
+if(x<0){
+    document.write("O número é negativo!");
+}else if(x>0){
+    document.write("O número é positivo!");
+}else{
+    document.write("O número é neutro");
+}
