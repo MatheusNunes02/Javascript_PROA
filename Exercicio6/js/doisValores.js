48a7d9fd464d0f93876cba39b1a3e7e25ec5dcfb
MatheusNunes02 <88735983+MatheusNunes02@users.noreply.github.com>
2021-09-21 19:51:52 -0300

    exercicio 6

---
+++ Exercicio6/js/doisValores.js
@@ -0,0 +1,10 @@
+let x = Number(prompt("Digite o primeiro número"));
+let y = Number(prompt("Digite o segundo número"));
+
+if(x==y){
+    document.write("Números iguais");
+}else if(x>y){
+    document.write("Primeiro maior");
+}else{
+    document.write("Segundo é maior");
+}
